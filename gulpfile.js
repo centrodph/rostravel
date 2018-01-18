34a770e299e01c9c67ae66154ac59638f3f91dd6
PERRUCCI ISRAEL <B040825@rio.ar.bsch>
2018-01-18 17:33:20 -0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require("gulp");
+var less = require("gulp-less");
+var autoprefixer = require("gulp-autoprefixer");
+var plumber = require("gulp-plumber");
+var browserSync = require("browser-sync").create();
+var $ = require("gulp-load-plugins")({
+  lazy: true
+});
+
+const config = {
+  less: "./less/*",
+  css: "./css/"
+};
+
+gulp.task("styles", function() {
+  return gulp
+    .src(config.less)
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 version", "> 5%"]
+      })
+    )
+    .pipe(gulp.dest(config.css))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("less-watcher", function() {
+  gulp.watch(config.less, ["styles"]);
+});
+
+/**
+ * Levantar el servidor de forma rapida.
+ */
+gulp.task("serveFast", ["styles"], function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch(config.less, ["styles"]);
+  gulp.watch("./*.html").on("change", browserSync.reload);
+});
