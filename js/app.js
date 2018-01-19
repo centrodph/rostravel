9bc0d7971a30d9beaea6e0291916db8e7c9622f9
PERRUCCI ISRAEL <B040825@rio.ar.bsch>
2018-01-19 17:51:09 -0300

    boxes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
 // Home page slider
-$(".owl-carousel").owlCarousel({
+$(".owl-carousel-home-banner").owlCarousel({
   loop: true,
   items: 1,
   dots: false,
   margin: 10,
   responsiveClass: true
+});
+
+// box slider
+$(".owl-carousel-box-slider").owlCarousel({
+  loop: true,
+  items: 1,
+  dots: true,
+  nav: true,
+  margin: 10,
+  responsiveClass: true,
+  navText: [
+    '<i class="fas fa-angle-left"></i>',
+    '<i class="fas fa-angle-right"></i>'
+  ]
 });
