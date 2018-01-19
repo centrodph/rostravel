c801ca9d8113ba0c2b23c5a203134e5c5ed5365c
PERRUCCI ISRAEL <B040825@rio.ar.bsch>
2018-01-19 11:50:31 -0300

    header structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@ var gulp = require("gulp");
 var less = require("gulp-less");
 var autoprefixer = require("gulp-autoprefixer");
 var plumber = require("gulp-plumber");
-var browserSync = require("browser-sync").create();
+var browserSync = require("browser-sync");
 var $ = require("gulp-load-plugins")({
   lazy: true
 });
+var reload = browserSync.reload;
 
 const config = {
   less: "./less/*",
@@ -23,7 +24,7 @@ gulp.task("styles", function() {
       })
     )
     .pipe(gulp.dest(config.css))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.stream({ reload: true }));
 });
 
 gulp.task("less-watcher", function() {
@@ -37,7 +38,8 @@ gulp.task("serveFast", ["styles"], function() {
   browserSync.init({
     server: {
       baseDir: "./"
-    }
+    },
+    injectChanges: true
   });
   gulp.watch(config.less, ["styles"]);
   gulp.watch("./*.html").on("change", browserSync.reload);
